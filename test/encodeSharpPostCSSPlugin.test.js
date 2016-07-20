3fedc985b4831e344a1b0d533cbaada73e2b071f
kisenka <qtuzov@gmail.com>
2016-07-20 18:22:43 +0300

    Update encodeSharp test

--- test/encodeSharpPostCSSPlugin.test.js
+++ test/encodeSharpPostCSSPlugin.test.js
@@ -2,9 +2,9 @@ require('chai').should();
 var postcss = require('postcss');
 var encodeSharp = require('../lib/encodeSharpPostCSSPlugin');
 
-function test(name, input, expected) {
+function test(name, options, input, expected) {
   it(name, function (done) {
-    postcss([encodeSharp()])
+    postcss([ encodeSharp(options) ])
       .process(input)
       .then(function (result) {
         result.css.should.be.eql(expected);
@@ -18,12 +18,14 @@ describe('Encode sharp in query string postcss plugin', function() {
 
   test(
     'should encode',
+    null,
     '.a {background: url(./image.svg?param=#qwe&param2=qwe);}',
     '.a {background: url(./image.svg?param=%23qwe&param2=qwe);}'
   );
 
   test(
     'should encode only query param values',
+    null,
     '.a {background: url(./image.svg?query#lalala);}',
     '.a {background: url(./image.svg?query#lalala);}'
   );

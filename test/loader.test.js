34cc711061d0f95a759bd828c88018759e5de454
kisenka <qtuzov@gmail.com>
2016-07-20 18:59:06 +0300

    Update loader & tests

--- test/loader.test.js
+++ test/loader.test.js
@@ -4,45 +4,86 @@ chai.should();
 chai.use(require('sinon-chai'));
 
 var loader = require('../lib/loader');
-var createMockedContext = require('./test-utils/createMockedContext');
+var parseQuery = loader.parseQuery;
+var createFakeContext = require('./test-utils/createFakeLoaderContext');
 
 /**
  * @param {String} content
  * @param {Object} context
  * @returns {Promise}
  */
-function runWithMockedContext(content, context) {
-  var context = createMockedContext(context);
+function runInFakeContext(content, context) {
+  var context = createFakeContext(context);
   loader.call(context, content);
   return context._promise;
 }
 
-describe('Fill loader', function() {
+describe('SVG fill loader', function() {
+
+  describe('parseQuery', function () {
+    it('should properly parse param values which contains comma', function() {
+      parseQuery('?query=a,b').should.eql({
+        query: 'a,b'
+      });
+    });
+  });
 
   it('should be cacheable', function (done) {
     var spy = sinon.spy(function() { return true });
 
-    runWithMockedContext('', { cacheable: spy })
+    runInFakeContext('', { cacheable: spy })
       .then(function () {
         spy.should.have.been.calledOnce;
         done();
-      });
+      })
+      .catch(done);
   });
 
   it('`raw` option is false by default', function(done) {
-    runWithMockedContext('<svg></svg>', {query: ''})
+    runInFakeContext('<svg></svg>', {query: ''})
       .then(function(result) {
         result.should.contain('module.exports');
         done();
-      });
+      })
+      .catch(done);
   });
 
   it('should handle `raw` option properly', function(done) {
-    runWithMockedContext('<svg></svg>', {query: '?raw=true'})
+    runInFakeContext('<svg></svg>', {query: '?raw=true'})
       .then(function(result) {
         result.should.not.contain('module.exports');
         done();
-      });
+      })
+      .catch(done);
+  });
+
+  it('should allow to configure default options via loader config', function(done) {
+    var input = '<path /><circle /><polygon />';
+    var expected = '<path fill="red" /><circle fill="red" /><polygon />';
+
+    runInFakeContext(input, {
+      query: '?raw&selector=path,circle&fill=red'
+    })
+      .then(function(result) {
+        result.should.eql(expected);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('should allow to mix loader config with resource query params', function(done) {
+    var input = '<path /><circle />';
+    var expected = '<path fill="blue" /><circle />';
+
+    runInFakeContext(input, {
+      query: '?raw&selector=path',
+      resourceQuery: '?fill=blue'
+    })
+      .then(function(result) {
+        result.should.eql(expected);
+        done();
+      })
+      .catch(done);
   });
 
 });

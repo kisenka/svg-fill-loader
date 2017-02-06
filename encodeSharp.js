fa2bd501a9565e6281278773543d68cfb3e005f7
kisenka <qtuzov@gmail.com>
2017-02-06 20:56:05 +0300

    encodeSharp loader: pass sourcemap to next loader (fixes #7)

--- encodeSharp.js
+++ encodeSharp.js
@@ -1,14 +1,14 @@
 var postcss = require('postcss');
 var encodeSharpPlugin = require('./lib/encodeSharpPostCSSPlugin');
 
-module.exports = function(content) {
+module.exports = function(content, sourcemap) {
   this.cacheable && this.cacheable();
   var done = this.async();
 
   postcss([ encodeSharpPlugin() ])
     .process(content)
     .then(function (result) {
-      done(null, result.css);
+      done(null, result.css, sourcemap);
     })
     .catch(done);
 };
